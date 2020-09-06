47d46b614fd042c4b5091627bcae86fd2d82f824
Shawn Lim <shawnlim89@live.com>
2020-09-06 22:05:30 +0800

    Remove extraneous text

--- src/pages/404.js
+++ src/pages/404.js
@@ -15,8 +15,6 @@ const NotFoundPage = () => (
         </p>
       </main>
     </main>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
